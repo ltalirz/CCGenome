abba36325bc0a37f03fa9d37179f560f1181f720
Leopold Talirz <leopold.talirz@gmail.com>
2019-02-01 14:31:22 +0100

    initial commit

---
+++ figure/config.py
@@ -0,0 +1,33 @@
+import collections
+import yaml
+from os.path import join, dirname
+
+static_dir = join(dirname(__file__), "static")
+
+with open(join(static_dir, "columns.yml"), 'r') as f:
+    quantity_list = yaml.load(f)
+
+quantities = collections.OrderedDict([(q['column'], q) for q in quantity_list])
+
+plot_quantities = [ q for q in quantities.keys() if quantities[q]['type'] == 'float' ]
+
+bondtype_dict = collections.OrderedDict([
+    ('0', "#1f77b4"),
+    ('1', "#d62728"),
+    ('2', "#ff7f0e"),
+    ('3', "#2ca02c"),
+    ('4', "#778899"),
+    ('5', "#04f238"),
+])
+
+with open(join(static_dir, "filters.yml"), 'r') as f:
+    filter_list = yaml.load(f)
+
+with open(join(static_dir, "presets.yml"), 'r') as f:
+    presets = yaml.load(f)
+
+for k in presets.keys():
+    if 'clr' not in presets[k].keys():
+        presets[k]['clr'] = presets['default']['clr']
+
+max_points = 70000
---
+++ prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Download jsmol
+wget https://sourceforge.net/projects/jmol/files/Jmol/Version%2014.29/Jmol%2014.29.22/Jmol-14.29.22-binary.zip/download --output-document jmol.zip
+unzip jmol.zip 
+cd jmol-14.29.22
+unzip jsmol.zip
+mv jsmol ../detail/static
+cd ..
+rm -r jmol-14.29.22
+
+## Download data
+cd data/
+#export base_url=http://archive.materialscloud.org/file/2018.0003/v2; \
+#    wget ${base_url}/structures.tgz &&\
+#    wget ${base_url}/properties.tgz
+
+# Extract data
+tar xf structures.tar.gz && rm structures.tag.gz
+cd ..
+
+# Create sqlite DB
+python import_db.py
